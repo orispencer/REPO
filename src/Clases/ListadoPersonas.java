6bdfea46f074eb967366db2feb55f3535ba63f9f
Orianny <Orianny@192.168.2.25>
2015-07-11 14:59:24 -0430

    Asignacion de tecnicas III

--- src/Clases/ListadoPersonas.java
+++ src/Clases/ListadoPersonas.java
@@ -1,10 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Clases;
 
+import Ventanas.CrearPersona;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -12,5 +10,25 @@ import java.util.List;
 clase que represeta el listado de personas.
 */
 public class ListadoPersonas {
-  private List<Persona> listadoPersonas;   
+  private List<Persona> listadoPersonas=new ArrayList();  
+
+    public ListadoPersonas(List<Persona> listadoPersonas) {
+        this.listadoPersonas = listadoPersonas;
+    }
+
+    public ListadoPersonas() {
+        }
+
+    public List<Persona> getListadoPersonas() {
+        return listadoPersonas;
+    }
+
+    public void setListadoPersonas(List<Persona> listadoPersonas) {
+        this.listadoPersonas = listadoPersonas;
+    }
+    
+    public List<Persona> CargarDatos(Persona p){
+    this.listadoPersonas.add(p);
+    return listadoPersonas;
+    }
 }
--- src/Clases/Persona.java
+++ src/Clases/Persona.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Clases;
+
+import Ventanas.CrearPersona;
 
 /*
 clase que representa una persona, puede realizar un registro 
@@ -12,4 +10,40 @@ public class Persona {
     private String cedula;//cedula de la persona
     private int  edad;//edad de la persona
     private String nombre;//nombre de la persona
+
+    public Persona(String cedula, int edad, String nombre) {
+        this.cedula = cedula;
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+    public Persona() {
+    }
+    
+    
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
 }
